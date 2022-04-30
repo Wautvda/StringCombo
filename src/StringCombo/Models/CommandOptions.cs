fd87e00944cba3ce2657a479fa17686d068ca622
wautvda <wautvda@gmail.com>
2022-04-30 16:08:58 +0200

    Add default input file

--- src/StringCombo/Models/CommandOptions.cs
+++ src/StringCombo/Models/CommandOptions.cs
@@ -4,6 +4,11 @@ namespace StringCombo.Models;
 
 public class CommandOptions
 {
-    [Value(index: 0, Required = true, HelpText = "File location with input text file.")]
-    public string Path { get; set; }
+    [Value(index: 0, Required = true, HelpText = "File location with input text file.", Default = "./Data/input.txt")]
+    public string Path { get; }
+    
+    public CommandOptions(string path)
+    {
+        Path = path;
+    }
 }
