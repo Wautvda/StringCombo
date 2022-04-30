8ca508cad018687ef98e2f4cb8511a8b7f02bb11
wautvda <wautvda@gmail.com>
2022-04-30 17:47:21 +0200

    Add combined string length parameter to command arguments

--- src/StringCombo/Models/CommandOptions.cs
+++ src/StringCombo/Models/CommandOptions.cs
@@ -4,11 +4,20 @@ namespace StringCombo.Models;
 
 public class CommandOptions
 {
-    [Value(index: 0, Required = true, HelpText = "File location with input text file.", Default = "./Data/input.txt")]
+    [Value(index: 0, Required = false, HelpText = "File location with input text file.", Default = "./Data/input.txt")]
     public string Path { get; }
     
-    public CommandOptions(string path)
+    [Option(shortName: 'l', longName: "length", Required = false, HelpText = "The length of the combined strings", Default = 6)]
+    public int Length { get; }
+
+    public CommandOptions(): this("./Data/input.txt", 6)
+    {
+        
+    }
+    
+    public CommandOptions(string path, int length)
     {
         Path = path;
+        Length = length;
     }
 }
